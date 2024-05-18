688c1fbb17d8fec69869945b354a4a4a109b64ca
dosel2691 <dosel2691@gmail.com>
2024-05-18 14:48:43 +0900

    add bottom

--- app/src/main/java/com/example/run/MainActivity.java
+++ app/src/main/java/com/example/run/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.run;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.run.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView mBottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        mBottomNavigationView = findViewById(R.id.navigationView);
+        mBottomNavigationView.setOnItemSelectedListener(item -> {
+            if(item.getItemId() == R.id.fragment_home) {
+
+            } else if (item.getItemId() == R.id.fragment_search) {
+
+            }
+            return true;
+        });
     }
 }
